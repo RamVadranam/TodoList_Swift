3d108d82be0ac383739d565ed790d7a20a57c885
ram-123 <ramchandra.vadranam@gmail.com>
2014-07-04 12:51:45 +0100

    initial Commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-                            
+class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet var tableView:UITableView!
+    
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!)
+        
+        {
+        if(tableView.endEditing(true)){
+            taskMgr.tasks.removeAtIndex(indexPath.row)
+            
+            tableView.reloadData()
+        }
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    {
+        return taskMgr.tasks.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "tCell")
+        
+        cell.textLabel.text=taskMgr.tasks[indexPath.row].name
+        cell.detailTextLabel.text=taskMgr.tasks[indexPath.row].desc
+        
+        return cell
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ To Do List/TaskManager.swift
@@ -0,0 +1,28 @@
+//
+//  TaskManager.swift
+//  To Do List
+//
+//  Created by Sriram Srinivasan on 02/07/2014.
+//  Copyright (c) 2014 Amsphere. All rights reserved.
+//
+
+import UIKit
+
+var taskMgr:TaskManager=TaskManager()
+
+struct task
+{
+    var name="No-Name"
+    var desc="No-Desc"
+}
+
+class TaskManager: NSObject {
+    
+   var tasks=task[]()
+    
+    func addTask(tName:NSString,tDesc:NSString)
+    {
+        tasks.append(task(name:tName,desc: tDesc))
+    }
+   
+}
